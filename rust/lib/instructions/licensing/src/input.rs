26bc5b92f4ae4c81e67eeca98fb767c76dad6e91
Drazen Patekar <dpatekar@gmail.com>
2025-09-22 14:41:16 +0200

    Add missing projects to the workspace cargo, remove old "licensing" crate

--- rust/lib/instructions/licensing/src/input.rs
+++
@@ -1,7 +0,0 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::pubkey::Pubkey;
-
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct DelegateLicenseInput {
-    pub delegate: Option<Pubkey>
-}
--- rust/lib/instructions/licensing/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod delegate;
-mod state;
-mod input;
-
-pub use delegate::process_delegate_license;
