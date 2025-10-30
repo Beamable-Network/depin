9d85f7de1b7c876d05d32fc58d6d4f0e2299b2ad
Drazen Patekar <dpatekar@gmail.com>
2025-10-30 09:43:22 +0100

    Change BMB address and decimals

--- rust/depin-core/src/constants/mod.rs
+++ rust/depin-core/src/constants/mod.rs
@@ -7,7 +7,7 @@ pub const NETWORK_ADMIN: Pubkey = solana_program::pubkey!("BMBazb5g4wVcNMcaPuxjV
 pub const LICENSE_ADMIN: Pubkey = solana_program::pubkey!("BMBazb5g4wVcNMcaPuxjVgMA8GmV65mY29ZxFRALEhdS");
 
 pub const USDC_MINT: Pubkey = solana_program::pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
-pub const BMB_MINT: Pubkey = solana_program::pubkey!("BMBc2jhS1otEzpXJH3UiTALQBQkpmx9m8TUW3zyMvKYJ");
+pub const BMB_MINT: Pubkey = solana_program::pubkey!("BMBtwz6LFDJVJd2aZvL5F64fdvWP3RPn4NP5q9Xe15UD");
 
 pub const CHECKER_TREE: Pubkey = solana_program::pubkey!("BMBcEkev9ZEMdqYUjNirrsB4E8uuVfcno7haTcLnwkXo");
 pub const WORKER_TREE: Pubkey = solana_program::pubkey!("BMBw28bkTSFicoKZGoYNJzWnSyUjDRcxr7Qoc5mCuxf5");
