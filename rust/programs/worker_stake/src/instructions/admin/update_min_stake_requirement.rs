2473a5336887375d684cbb102b8d91d07f17d353
Drazen Patekar <dpatekar@gmail.com>
2025-11-21 14:24:26 +0100

    Remove unused import of ProgramError in update_min_stake_requirement.rs

--- rust/programs/worker_stake/src/instructions/admin/update_min_stake_requirement.rs
+++ rust/programs/worker_stake/src/instructions/admin/update_min_stake_requirement.rs
@@ -6,7 +6,6 @@ use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
     msg,
-    program_error::ProgramError,
     pubkey::Pubkey,
 };
 use crate::{
